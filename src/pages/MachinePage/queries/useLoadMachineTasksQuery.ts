fc898a0dcc58ac847377b306fbd0cff1a6eaf442
gerardramonp <gerardramonp@gmail.com>
2021-09-16 21:21:43 +0200

    Add comment

--- src/pages/MachinePage/queries/useLoadMachineTasksQuery.ts
+++ src/pages/MachinePage/queries/useLoadMachineTasksQuery.ts
@@ -5,6 +5,7 @@ import { getTaskByMachineId } from "../../../mocks/tasksRepository";
 import { getMachineByName } from "../../../mocks/machineRepository";
 
 export default function useLoadMachineTasksQuery(machineName: string) {
+  // el useQuery reb un array amb el nom de la "action" i les variables que si canvien torna a executar
   return useQuery(["loadMachineTasks", machineName], async () => {
     // TODO: const machine = await axios.get('/api/machine/name/${machineName}')
     const machine = getMachineByName(machineName);
