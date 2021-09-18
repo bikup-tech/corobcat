91b6dc81d03f3ed3bb336aaff92cedec74ee717b
gerardramonp <gerardramonp@gmail.com>
2021-09-18 11:20:57 +0200

    Add getTaskByUserId function in taskRepo

--- src/mocks/tasksRepository.ts
+++ src/mocks/tasksRepository.ts
@@ -3,3 +3,7 @@ import tasks from "./tasks.json";
 export function getTaskByMachineId(machineId: string) {
   return tasks.filter((task) => task.machineId === machineId);
 }
+
+export function getTaskByUserId(userId: string) {
+  return tasks.filter((task) => task.userId === userId);
+}
