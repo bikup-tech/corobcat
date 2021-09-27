22a47b02ea123779dc842b0193d2bc0c0d8f4c37
mourad5000 <mouradmounim7@gmail.com>
2021-09-27 17:23:40 +0200

    Create EmployeeCard component

--- src/components/MUITable/EnhancedEmployeeTable/EnhancedEmployeeTable.tsx
+++ src/components/MUITable/EnhancedEmployeeTable/EnhancedEmployeeTable.tsx
@@ -28,6 +28,14 @@ const StyledTableWrapper = styled.div`
   width: 100%;
   max-width: ${maxWidth}px;
 
+  .MuiTableHead-root {
+    background-color: red;
+  }
+
+  .MuiTableRow-head {
+    background: red;
+  }
+
   .MuiTableCell-head {
     color: #002740;
     font-weight: 700;
@@ -85,9 +93,7 @@ interface IEnhnacedEmployeeTasksTableProps {
   headCells: THeadCell[];
 }
 
-export default function EnhnacedEmployeeTasksTable(
-  props: IEnhnacedEmployeeTasksTableProps
-) {
+export default function EnhnacedEmployeeTasksTable(props: IEnhnacedEmployeeTasksTableProps) {
   const { tasks, headCells, isLoading, isError } = props;
 
   const [page, setPage] = React.useState(0);
@@ -119,13 +125,13 @@ export default function EnhnacedEmployeeTasksTable(
               <TableBody>
                 {isLoading ? (
                   <TableRow>
-                    <TableCell colSpan={999}>
-                      <StyledLoadingWrapper>
-                        <CircularProgress />
-                        Cargando Datos...
-                      </StyledLoadingWrapper>
-                    </TableCell>
-                  </TableRow>
+                  <TableCell colSpan={999}>
+                    <StyledLoadingWrapper>
+                      <CircularProgress />
+                      Cargando Datos...
+                    </StyledLoadingWrapper>
+                  </TableCell>
+                </TableRow>
                 ) : isError ? (
                   <></>
                 ) : (
---
+++ src/pages/EmployeesListPage/components/EmployeesListPage/EmployeesListPage.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+import "./EmployeesListPage.scss";
+
+// types
+import { TUserResponse } from "../../../../types/employeeTypes";
+
+// components
+import EmployeeCard from "../../EmployeeCard/EmployeeCard";
+import { CircularProgress } from "@mui/material";
+
+const StyledEmployeePageContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: start;
+  align-items: flex-start;
+  width: 100%;
+  height: 100%;
+`;
+
+const StyledLoadingWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+
+  font-size: 1.75rem;
+`;
+
+interface IEmployeesCardProps {
+  employees: TUserResponse[] | undefined;
+  isLoading: boolean;
+  isError: boolean;
+}
+
+function EmployeesListPage(props: IEmployeesCardProps) {
+  const { employees, isLoading, isError } = props;
+  // TODO: que la carta sea totalmente estirada
+  return (
+    <>
+      <div className="employees-title">Lista de empleados</div>
+      <StyledEmployeePageContainer className="employees-container">
+        {isLoading ? (
+          <StyledLoadingWrapper>
+            <CircularProgress />
+            Cargando Datos...
+          </StyledLoadingWrapper>
+        ) : isError ? (
+          <></>
+        ) : (
+          employees &&
+          employees.map((employee) => {
+            return <EmployeeCard key={employee._id} employee={employee} />;
+          })
+        )}
+      </StyledEmployeePageContainer>
+    </>
+  );
+}
+export default EmployeesListPage;
