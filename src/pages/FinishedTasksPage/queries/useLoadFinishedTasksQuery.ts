a7b46162e2f929c22c4d487de83d69b619d960c6
gerardramonp <gerardramonp@gmail.com>
2021-09-27 18:39:22 +0200

    Add query refetching to machines, finishedTasks & employeeTasks

--- src/pages/FinishedTasksPage/queries/useLoadFinishedTasksQuery.ts
+++ src/pages/FinishedTasksPage/queries/useLoadFinishedTasksQuery.ts
@@ -5,10 +5,16 @@ import { getFinishedTasks } from "../../../mocks/tasksRepository";
 
 export default function useFinisedTasksQuery() {
   // el useQuery reb un array amb el nom de la "action" i les variables que si canvien torna a executar
-  return useQuery(["loadFinishedTasks"], async () => {
-    // TODO: const machine = await axios.get('/api/machine/name/${machineName}')
-    const data = getFinishedTasks();
+  return useQuery(
+    ["loadFinishedTasks"],
+    async () => {
+      // TODO: const machine = await axios.get('/api/machine/name/${machineName}')
+      const data = getFinishedTasks();
 
-    return data;
-  });
+      return data;
+    },
+    {
+      refetchInterval: 60000,
+    }
+  );
 }
--- src/pages/MachinePage/queries/useLoadMachineTasksQuery.ts
+++ src/pages/MachinePage/queries/useLoadMachineTasksQuery.ts
@@ -9,15 +9,21 @@ export default function useLoadMachineTasksQuery(
   forceRender: number
 ) {
   // el useQuery reb un array amb el nom de la "action" i les variables que si canvien torna a executar
-  return useQuery(["loadMachineTasks", machineName, forceRender], async () => {
-    // TODO: const machine = await axios.get('/api/machine/name/${machineName}')
-    const machine = getMachineByName(machineName);
+  return useQuery(
+    ["loadMachineTasks", machineName, forceRender],
+    async () => {
+      // TODO: const machine = await axios.get('/api/machine/name/${machineName}')
+      const machine = getMachineByName(machineName);
 
-    // TODO: const {data} = await axios.get('/api/tasks/${machineName}')
-    if (machine) {
-      const data = getTaskByMachineId(machine._id);
+      // TODO: const {data} = await axios.get('/api/tasks/${machineName}')
+      if (machine) {
+        const data = getTaskByMachineId(machine._id);
 
-      return data;
+        return data;
+      }
+    },
+    {
+      refetchInterval: 60000,
     }
-  });
+  );
 }
