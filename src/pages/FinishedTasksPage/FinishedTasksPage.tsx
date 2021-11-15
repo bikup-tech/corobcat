36020ac97114916c8ef1b5af44d7ecf7cd290878
gcsaperas-konodrac <90689726+gcsaperas-konodrac@users.noreply.github.com>
2021-11-15 20:33:55 +0100

    # Filter tasks by date functionality

--- src/pages/FinishedTasksPage/FinishedTasksPage.tsx
+++ src/pages/FinishedTasksPage/FinishedTasksPage.tsx
@@ -1,7 +1,16 @@
+import { useState, useEffect } from "react";
 import EnhancedFinishedTasksTable from "../../components/MUITable/EnhancedFinishedTasksTable/EnhancedFinishedTasksTable";
 import FinishedTasksInfoHeader from "./components/FinishedTasksInfoHeader";
 import { THeadCell } from "../../components/MUITable/MUITableTypes";
 import { TTaskResponse } from "../../types/taskTypes";
+import FinishedTasksFilterHeader from "./components/FinishedTasksFilterHeader";
+import { Moment } from "moment";
+import moment from "moment";
+
+export interface DateState {
+  startDate: Moment | null;
+  endDate: Moment | null;
+}
 
 interface IFinishedTasksPageProps {
   tasks: TTaskResponse[] | undefined;
@@ -12,6 +21,42 @@ interface IFinishedTasksPageProps {
 
 function FinishedTasksPage(props: IFinishedTasksPageProps) {
   const { tasks, isLoading, isError } = props;
+
+  /** date state for tasks filtering */
+  const [dates, setDates] = useState<DateState>({
+    startDate: null,
+    endDate: null,
+  });
+  /** tasks filtered by date */
+  const [filteredTasks, setFilteredTasks] = useState(tasks);
+
+  useEffect(() => {
+    if (dates.startDate && dates.endDate) {
+      const { startDate, endDate } = dates;
+      filterTasksByDateRange(startDate, endDate);
+    } else {
+      setFilteredTasks(tasks);
+    }
+  }, [dates]);
+
+  /**
+   *
+   * Function that filters tasks by a start and end date.
+   *
+   * It returns tasks finished between that dates.
+   *
+   * @param startDate
+   * @param endDate
+   */
+  function filterTasksByDateRange(startDate: Moment, endDate: Moment) {
+    const filteredTasks = tasks?.filter((task) => {
+      const taskEnd = moment(task.end);
+      if (taskEnd >= startDate && taskEnd <= endDate) {
+        return true;
+      }
+    });
+    setFilteredTasks(filteredTasks);
+  }
 
   const headCells: THeadCell[] = [
     {
@@ -47,9 +92,10 @@ function FinishedTasksPage(props: IFinishedTasksPageProps) {
   return (
     <>
       <FinishedTasksInfoHeader tasks={tasks} />
+      <FinishedTasksFilterHeader filteredTasks={filteredTasks} dates={dates} setDates={setDates} />
       <EnhancedFinishedTasksTable
         headCells={headCells}
-        tasks={tasks}
+        tasks={filteredTasks}
         isLoading={isLoading}
         isError={isError}
       />
